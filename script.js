673ec477c8c518a45f8c5f2852f1800df9abb2a1
Moufles-PG <morgangp@protonmail.com>
2026-08-24 17:45:33 +0200

    Update script.js

--- script.js
+++ script.js
@@ -100,7 +100,7 @@ function positionInitialeGrille() {
         la grille de 20 px.
     */
 
-    const margeSupplementaire = +55;
+    const margeSupplementaire = +45;
 
     const basLigne =
         rectClavier.top -
